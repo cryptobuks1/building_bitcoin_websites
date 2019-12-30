28bad6bbb8e9afd01b9b9407802a4d00b1d255fd
Graham <s3242096@student.rmit.edu.au>
2019-12-30 17:59:50 +0800

    Initial commit

---
+++ exchange_rate/index.php
@@ -0,0 +1,26 @@
+<?php
+    $url = "https://api.coinbase.com/v2/prices/spot?currency=USD";
+    $fileGet = file_get_contents($url);
+    $json = json_decode($fileGet, TRUE);
+?>
+<! DOCTYPE html>
+<html>
+    <head>
+        <title>BTC Price</title>
+        <link href="https://fonts.googleapis.com/css?family=Ubuntu:700italic" rel="stylesheet" type="text/css">
+        <style>
+            #container 
+            {
+                font-size: 40px;
+                font-family: 'Ubuntu', sans-serif;
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <div id="container">
+            <img src="./img/bitcoin.png" alt="Bitcoin">bitcoin<br>
+            $<?php echo $json['data']['amount']; ?>
+        </div>
+    </body>
+</html>
