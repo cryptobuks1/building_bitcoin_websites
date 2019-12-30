a843bddcd562c38333e6cc9cb3b0e2dafaaebf36
Graham <s3242096@student.rmit.edu.au>
2019-12-30 20:33:52 +0800

    timezone

--- price_widget/index.php
+++ price_widget/index.php
@@ -1,7 +1,7 @@
 <?php
     // Store API result in database, script to query database.
     // This will prevent exceeding API call rate limits.
-
+    date_default_timezone_set('Australia/Perth');
     $date = date("d/m/Y - h:i:sa");
 
     $url = "https://www.bitstamp.net/api/ticker/";
@@ -64,21 +64,20 @@
         </style>
     </head>
     <body>
-            <div id="container">
-                <table width="100%">
-                    <tr>
-                        <td rowspan="4" width="60%" id="lastPrice">$ <?php echo $lastPrice; ?></td>
-                        <td align="right" style="color: <?php echo $color; ?>;"><?php echo $percentChange; ?> %</td>
-                    </tr>
-                    <td align="right">H $ <?php echo $highPrice; ?></td>
-                    <tr>
-                        <td align="right">L $ <?php echo $lowPrice; ?></td>
-                    </tr>
-                    <tr>
-                        <td align="right" colspan="2" id="dateTime"><?php echo $date; ?></td>
-                    </tr>
-                </table>
-            </div>
-            <?php var_dump($json); ?>
+        <div id="container">
+            <table width="100%">
+                <tr>
+                    <td rowspan="4" width="60%" id="lastPrice">$ <?php echo $lastPrice; ?></td>
+                    <td align="right" style="color: <?php echo $color; ?>;"><?php echo $percentChange; ?> %</td>
+                </tr>
+                <td align="right">H $ <?php echo $highPrice; ?></td>
+                <tr>
+                    <td align="right">L $ <?php echo $lowPrice; ?></td>
+                </tr>
+                <tr>
+                    <td align="right" colspan="2" id="dateTime"><?php echo $date; ?></td>
+                </tr>
+            </table>
+        </div>
     </body>
 </html>
